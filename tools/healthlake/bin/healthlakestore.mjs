6c4ced912422fbe76aea82de834f4268b7e42000
Bill Wallace <wayfarer3130@gmail.com>
2022-11-24 11:14:06 -0500

    Move tools to static-dicomweb

--- tools/healthlake/bin/healthlakestore.mjs
+++
@@ -1,4 +0,0 @@
-import { curieStoreConfig, configureProgram } from "../lib/index.mjs";
-
-// Configure program commander and run the action
-configureProgram(curieStoreConfig);
--- tools/healthlake/jest.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from "../../.config/jest/jest.config.js";
-
-export default {...baseConfig,
-  preset: 'ts-jest',
-  transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    "^.+\\.(js|mjs)$": "babel-jest",
-  },
-};
