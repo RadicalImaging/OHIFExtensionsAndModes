97ebc1d28103de421db2b8e2a7dcc9d21a81e233
md-prog <26860200+md-prog@users.noreply.github.com>
2022-12-07 09:12:56 -0500

    build fixes for mode-dicom-html

--- ohif-configurable/mode-dicom-html/.webpack/webpack.prod.js
+++ ohif-configurable/mode-dicom-html/.webpack/webpack.prod.js
@@ -1,50 +1,97 @@
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 const pkg = require('../package.json');
-const webpackCommon = require('../../../.webpack/webpack.base.js');
 
-const ROOT_DIR = path.join(__dirname, './../');
-const SRC_DIR = path.join(__dirname, '../src');
-const DIST_DIR = path.join(__dirname, '../dist', pkg.name);
+const rootDir = path.resolve(__dirname, '../');
+const outputFolder = path.join(__dirname, `../dist/umd/${pkg.name}/`);
+const outputFile = 'index.umd.js';
 
-const fileName = 'index.umd.js';
-module.exports = (env, argv) => {
-  const commonConfig = webpackCommon(env, argv, { SRC_DIR, DIST_DIR });
-
-  return merge(commonConfig, {
-    stats: {
-      colors: true,
-      hash: true,
-      timings: true,
-      assets: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false,
-      children: false,
-      warnings: true,
+const config = {
+  mode: 'production',
+  entry: rootDir + '/' + pkg.module,
+  devtool: 'source-map',
+  output: {
+    path: outputFolder,
+    filename: outputFile,
+    library: pkg.name,
+    publicPath: `/umd/${pkg.name}/`,
+    libraryTarget: 'umd',
+    chunkFilename: '[name].chunk.js',
+    umdNamedDefine: true,
+  },
+  externals: [
+    {
+      "cornerstone-wado-image-loader": {
+        root: 'cornerstone-wado-image-loader',
+        commonjs2: 'cornerstone-wado-image-loader',
+        commonjs: 'cornerstone-wado-image-loader',
+        amd: 'cornerstone-wado-image-loader',
+      },
+      'react': {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react',
+      },
+      'config-point': {
+        root: 'config-point',
+        commonjs2: 'config-point',
+        commonjs: 'config-point',
+        amd: 'config-point',
+      },
+      'classnames': {
+        root: 'classnames',
+        commonjs2: 'classnames',
+        commonjs: 'classnames',
+        amd: 'classnames',
+      },
+      'react-router-dom': {
+        root: 'react-router-dom',
+        commonjs2: 'react-router-dom',
+        commonjs: 'react-router-dom',
+        amd: 'react-router-dom',
+      },
+      'dicomweb-client': {
+        root: 'dicomweb-client',
+        commonjs2: 'dicomweb-client',
+        commonjs: 'dicomweb-client',
+        amd: 'dicomweb-client',
+      },
+      dcmjs: {
+        root: 'dcmjs',
+        commonjs2: 'dcmjs',
+        commonjs: 'dcmjs',
+        amd: 'dcmjs',
+      },
+      '@ohif/core': {
+        commonjs2: '@ohif/core',
+        commonjs: '@ohif/core',
+        amd: '@ohif/core',
+        root: '@ohif/core',
+      },
+      '@ohif/ui': {
+        commonjs2: '@ohif/ui',
+        commonjs: '@ohif/ui',
+        amd: '@ohif/ui',
+        root: '@ohif/ui',
+      },
     },
-    optimization: {
-      minimize: true,
-      sideEffects: true,
-    },
-    output: {
-      path: ROOT_DIR,
-      library: 'OHIFExtCornerstone',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      filename: pkg.main,
-    },
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 1,
-      }),
-      new MiniCssExtractPlugin({
-        filename: './dist/@radicalimaging/mode-dicom-html/[name].css',
-        chunkFilename: './dist/@radicalimaging/mode-dicom-html/[id].css',
-      }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js|\.tsx|\.ts)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+        resolve: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      },
     ],
-  });
+  },
+  resolve: {
+    modules: [path.resolve('../node_modules'), path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js', '.jsx', '.tsx', '.ts'],
+  },
 };
+
+module.exports = config;
