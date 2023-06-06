1be511487ce3e94e9477d8e9f8806e983acfae87
Bill Wallace <wayfarer3130@gmail.com>
2023-06-06 12:47:31 -0400

    fix(hangingProtocol): mn grid fixes and miscellaneous improvements (#36)
    * Fix hanging protocol mnGrid
    
    * Fixes for coding values as well
    
    * Fix the context menu registration
    
    * fix(hangingProtocol): Make the mnGrid hanging protocol default
    
    Misc other improvements

---
+++ ohif-configurable/config-mode/src/isValidMode.ts
@@ -0,0 +1,15 @@
+/** Any modality but SM or ECG */
+export default function isValidMode({ modalities }) {
+  const modalities_list = modalities.split('\\');
+  const validSingletonModalities = this.validSingletonModalities || ['SR'];
+  if( modalities_list.every(modality => validSingletonModalities.indexOf(modality)!==-1) ) {
+    return true;
+  }
+
+  const invalidModalities = this.invalidModalities || ['SM', 'ECG'];
+
+  // Slide Microscopy modality not supported by basic mode yet
+  return (
+    modalities_list.filter(it => invalidModalities.indexOf(it)===-1 ).length > 0
+  );
+};
--- ohif-configurable/config-mode/src/onModeEnter.ts
+++ ohif-configurable/config-mode/src/onModeEnter.ts
@@ -1,8 +1,10 @@
+const asArray = (v) => Array.isArray(v) ? v : [v];
+
 export default function onModeEnter({ servicesManager, extensionManager, commandsManager }) {
   const {
     measurementService,
-    ToolBarService,
-    ToolGroupService,
+    toolbarService,
+    toolGroupService,
     customizationService,
   } = servicesManager.services;
 
@@ -15,8 +17,8 @@ export default function onModeEnter({ servicesManager, extensionManager, command
   let unsubscribe;
 
   const activateTool = () => {
-    ToolBarService.recordInteraction(this.defaultTool);
-
+    asArray(this.defaultTool).forEach(tool => toolbarService.recordInteraction(tool));
+    
     // We don't need to reset the active tool whenever a viewport is getting
     // added to the toolGroup.
     unsubscribe();
@@ -24,15 +26,15 @@ export default function onModeEnter({ servicesManager, extensionManager, command
 
   // Since we only have one viewport for the basic cs3d mode and it has
   // only one hanging protocol, we can just use the first viewport
-  ({ unsubscribe } = ToolGroupService.subscribe(
-    ToolGroupService.EVENTS.VIEWPORT_ADDED,
+  ({ unsubscribe } = toolGroupService.subscribe(
+    toolGroupService.EVENTS.VIEWPORT_ADDED,
     activateTool
   ));
 
-  ToolBarService.init(extensionManager);
-  ToolBarService.addButtons(this.toolbarButtons);
+  toolbarService.init(extensionManager);
+  toolbarService.addButtons(this.toolbarButtons);
   Object.keys(this.toolBarSections).forEach(section => {
-    ToolBarService.createButtonSection(section, this.toolBarSections[section]);
+    toolbarService.createButtonSection(section, this.toolBarSections[section]);
   });
 
   customizationService?.addModeCustomizations?.(this.modeCustomizations);
